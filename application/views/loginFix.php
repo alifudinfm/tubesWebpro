8edab64eda56964f9c53527f0eb5efcfb3f251b8
alifudinfm <alifudinfajar@gmail.com>
2019-11-23 18:36:29 +0700

    revisi login.php

--- application/views/loginFix.php
+++ application/views/loginFix.php
@@ -18,21 +18,20 @@
 		<div  style="width : 300px">
   		<div class="form-group">
  		<label for="exampleInputEmail1">User</label>
-    	<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    	<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    	<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
   	</div>
   	<div class="form-group">
     	<label for="pwd">Password</label>
-    	<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    	<input type="password" class="form-control" id="exampleInputPassword1" >
   	</div>
 	  <button type="submit" color="black" class="btn btn-dark">Login</button>
 	</div>
 	<a href="#">Lupa password?</a><br>
-	<span>*if you have a problem with login, try clear cookie first</span>
+	<small>*if you have a problem with login, try clear cookie first</small>
 	</center>
 </form>
 	</div>
-	<?php $this->load->view('_partials/sidebar.php') ?>
+	<?php $this->load->view('_partials/sidebar.php'); ?>
 </div>
 </div>
 
@@ -40,6 +39,5 @@
 
 
 </body>
-<?php $this->load->view('_partials/footer.php');
- ?>
+<?php $this->load->view('_partials/footer.php');?>
 </html>
